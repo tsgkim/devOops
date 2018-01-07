0da46910c018af5ef210ac5d8b8e250524e22c3f
shiguang.tu <shiguang.tu@andpay.me>
2018-01-07 23:08:45 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,90 @@
+# devOops
+
+服务、进程监听，挂掉自动重启，并用阿里大于短信自动通知
+
+在 https://github.com/glennzw/DevOops 上改版而成，感谢原作者
+
+# 使用说明：
+#### 安装 python、pip 
+
+#### git clone 本项目
+
+#### 配置 services.json
+```
+eg: 配置 nginx
+     {
+       # 短信通知是哪个服务
+       "name" : "nginx", 
+       # 服务真正名称，必须和服务名一样
+       "proc" : "nginx",
+       # 服务启动命令，必须和服务启动命令一样
+       "restart": "service nginx start"
+     }
+```
+
+#### 配置 pid.json
+```
+eg: 配置 jenkins
+    说明：jenkins 是一个 war 包，在tomcat容器中运行
+      {
+        # 短信通知是哪个进程
+        "name" : "jenkins",
+        # 进程真正名称，必须和进程名一样
+        "proc" : "jenkins",
+        # 进程启动脚本，必须和进程启动命令一样（就是 怎样启动 这个 jenkins 所在的 tomcat 命令，我这里封装了一个脚本，所以写成这样）
+        "restart": "sh /script/jenkins/start.sh"
+       }
+```
+       
+#### 配置 阿里大于短信通知、程序定时执行配置
+```
+[ali]
+# 阿里大于短信发送 app-key
+key: 
+# 阿里大于短信发送 app-secret
+secret:
+# 阿里大于短信发送 签名名称
+sign: 
+# 默认，无需配置
+type: normal
+# 接收短信手机号
+phone: 
+# 阿里大于短信发送模板变量
+username: tsgkim
+# 服务异常通知短信模板
+notice-template: 
+# 服务重启成功短信模板
+success-template: 
+# 服务重启失败短信模板
+error-template: 
+
+[Other]
+# 设置多长时间重新监听服务、进程
+Interval: 10
+```
+因为程序中配置了下面模板中的变量，请务必按照此模板配置在你的阿里大于上面，如果你能改程序，请随意配置...
+
+服务异常通知短信模板
+
+![服务异常通知短信模板](http://resource.tsg.kim/%E6%9C%8D%E5%8A%A1%E7%9B%91%E5%90%AC%E9%80%9A%E7%9F%A5.png)
+
+服务重启成功短信模板
+
+![服务重启成功短信模板](http://resource.tsg.kim/%E6%9C%8D%E5%8A%A1%E7%9B%91%E5%90%AC%E6%88%90%E5%8A%9F%E9%80%9A%E7%9F%A5.png)
+
+服务重启失败短信模板
+
+![服务重启失败短信模板](http://resource.tsg.kim/%E6%9C%8D%E5%8A%A1%E9%87%8D%E5%90%AF%E5%A4%B1%E8%B4%A5%E9%80%9A%E7%9F%A5.png)
+          
+#### 安装 必要的包
+```
+说明：如果下面包有，请忽略此步骤，如果后台运行 devOops.py 提示缺少包，请按照下面示例安装 -->  pip install 缺少的包名
+pip install top
+pip install psutil
+```
+
+#### 后台运行 devOops.py
+```
+ nohup python -u devOops.py > /var/log/python/devOops.log 2>&1 &
+ 运行日志存放位置： /var/log/python/devOops.log
+```
---
+++ devOops.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import top.api
+import psutil
+import time
+import ConfigParser
+from subprocess import call, STDOUT
+import subprocess
+import signal
+import sys
+import json
+import os
+import requests
+import sys
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+DEBUG_MODE = False #Won't dispatch SMSs
+
+requests.packages.urllib3.disable_warnings() # http://stackoverflow.com/questions/29099404/ssl-insecureplatform-error-when-using-requests-package
+
+#Parse configuration options
+if DEBUG_MODE:
+    print "[+] Starting DevOops monitor in debug mode (no SMS will be dispatched)"
+else:
+    print "[+] Starting DevOops monitor"
+
+print "[-] Loading config file"
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+key = Config.get('ali','key')
+secret = Config.get('ali','secret')
+username = Config.get('ali','username')
+sign = Config.get('ali','sign')
+type = Config.get('ali','type')
+phone = Config.get('ali','phone')
+phone = Config.get('ali','phone')
+notice-template = Config.get('ali','notice-template')
+success-template = Config.get('ali','success-template')
+error-template = Config.get('ali','notice-template')
+
+INTERVAL = int(Config.get('Other', 'Interval'))  #Check every n seconds
+FNULL = open(os.devnull, 'w')
+
+#Read service monitoring file
+print "[-] Loading Services file"
+with open('services.json') as json_data:
+    SERVICES = json.load(json_data)
+
+print "[-] Loading Pid file"
+with open('pid.json') as pid_data:
+    PIDS = json.load(pid_data)
+
+def isServiceRunning(name):
+  "Check if a process name is running"
+  for proc in psutil.process_iter():
+    if proc.name() == name:
+      return True
+  return False
+
+def isPidRunning(name):
+  "Check if a pid name is running"
+  res = subprocess.Popen("ps -ef | grep %s" % name,stdout=subprocess.PIPE,shell=True)
+  tomcats=res.stdout.readlines()
+  counts=len(tomcats)
+  if counts == 2:
+    return False
+  return True
+
+def notify(param, template):
+  req = top.api.AlibabaAliqinFcSmsNumSendRequest()
+  req.set_app_info(top.appinfo(key, secret))
+
+  req.sms_type = type
+  req.sms_free_sign_name = sign
+  req.sms_param = param
+  req.rec_num = phone
+  req.sms_template_code = template
+  try :
+       resp = req.getResponse()
+       dt=datetime.datetime.now()
+       print (resp)
+  except Exception,e:
+       dt=datetime.datetime.now()
+       print (e)
+
+  return resp
+
+def main():
+    print "[+] Monitoring services...."
+    #Inital pass through file to make sure services are running
+    for s in SERVICES:
+        if not isServiceRunning(s.get("proc")):
+          print "[!] At least one service in your services.json is not already running. Please ensure services are already running before starting."
+          exit(1)
+
+    for p in PIDS:
+        if not isPidRunning(p.get("proc")):
+          print "[!] At least one pid in your services.json is not already running. Please ensure pid are already running before starting."
+          exit(1)
+
+    while True:
+      mem = str(psutil.virtual_memory().percent) #Percent mem used
+      cpu = str(psutil.cpu_percent())
+
+      for s in SERVICES:
+        name, proc, restart = s.get("name"), s.get("proc"), s.get("restart")
+        if not isServiceRunning(proc):
+          print "[*] %s has stopped. Dispatching SMS." % name
+          if restart:
+            notify("{name:%s,service:%s,cpu:%s%%,ram:%s%%}" %(username,name,cpu,mem),notice-template)
+            r = call(restart.split(), stdout=FNULL, stderr=STDOUT)
+            time.sleep(10)
+            if isServiceRunning(proc):
+                notify("{name:%s,service:%s}" %(username,name),success-template)
+                print "[-] Successfully restarted %s" % name
+            else:
+                notify("{name:%s,service:%s}" %(username,name),error-template)
+                print "[-] Failed to restart %s" % name
+
+      for p in PIDS:
+        name, proc, restart = p.get("name"), p.get("proc"), p.get("restart")
+        if not isPidRunning(proc):
+          print "[*] %s has stopped. Dispatching SMS." % name
+          if restart:
+            notify("{name:%s,service:%s,cpu:%s%%,ram:%s%%}" %(username,name,cpu,mem),notice-template)
+            subprocess.Popen(restart,shell=True)
+            time.sleep(120)
+            if isPidRunning(proc):
+                notify("{name:%s,service:%s}" %(username,name),success-template)
+                print "[-] Successfully restarted %s" % name
+            else:
+                notify("{name:%s,service:%s}" %(username,name),error-template)
+                print "[-] Failed to restart %s" % name
+
+
+      time.sleep(INTERVAL)
+
+if __name__ == "__main__":
+    main()
